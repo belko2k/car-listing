618637d55613d80888a48f7df9cbcb67c2c05d38
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2024-02-09 18:44:43 +0100

    user creation

--- actions/register.ts
+++ actions/register.ts
@@ -1,17 +1,58 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
 
-  if (!validateFields) {
+  if (!validateFields.success) {
     return {
       error: 'Invalid fields!',
     };
   }
 
-  return { success: 'Email sent!' };
+  const { email, password, username, contactNumber, address } =
+    validateFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUserEmail = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUserEmail) {
+    return {
+      error: 'Email already in use!',
+    };
+  }
+
+  const existingUserUsername = await db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (existingUserUsername) {
+    return {
+      error: 'Username already taken!',
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      username,
+      password: hashedPassword,
+      contactNumber,
+      address,
+    },
+  });
+
+  return { success: 'Account created!' };
 };
